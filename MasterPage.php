a6d14ac9aa6070394c4550efbf80b0b63323900c
saranpong <creadz@gmail.com>
2021-04-12 09:39:25 +0700

    update detail

--- MasterPage.php
+++ MasterPage.php
@@ -13,10 +13,10 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, user-scalable=no" />
     <meta name="description" content="">
     <meta name="author" content="">
-
+	<link rel="manifest" href="manifest.json" />
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
     <title>nut.in.th</title>
@@ -25,7 +25,7 @@
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
@@ -120,22 +120,6 @@
     </nav>
  		';
  	}
-	
-	public static function GoogleAnalyst($id)
-	{
-		// UA-29751220-2
-		echo "
-			<script>
-			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			  ga('create', '",$id,"', 'auto');
-			  ga('send', 'pageview');
-			</script>
-		";
-	}
 
 
  }
--- index.php
+++ index.php
@@ -5,18 +5,32 @@
 
     MasterPage::Header();
     MasterPage::Navigation();
-	MasterPage::GoogleAnalyst("UA-29751220-2");
-	
 ?> 
+<style>
+	.img-responsive {
+		margin: 0 auto;
+	}
+</style>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-29751220-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
     <!-- Intro Header -->
-    <header class="intro">
+	
+    <header id="intro" class="intro">
         <div class="intro-body">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <h1 class="brand-heading">nut.in.th</h1>
-                        <p class="intro-text">developer, cat owner</p>
+                        <p class="intro-text">Software Developer</p>
                         <a href="#about" class="btn btn-circle page-scroll">
                             <i class="fa fa-angle-double-down animated"></i>
                         </a>
@@ -33,12 +47,16 @@
                 <h2>About Me</h2>
                 <div class="row">
                     <div class="col-lg-3 col-md-3 col-sm-3">
-                        <img alt="me" src="img/profile.png" class="img img-responsive">
+                        <img alt="me" src="img/profile.jpg" class="img img-responsive img-circle">
                     </div>
                     <div class="col-lg-9 col-md-9 col-sm-9">
-                        <p>My name is Saranpong (Nat). I'm a developer at Bangkok Airways. I live in Bangkok. This is my website. </p>
-                        <p>My work is about coding, analyze a problem and find solution with the teams. learn a new technology and new things.</p>
+                        <p>My name is Saranpong (Nat). I'm a Software Developer.</p>
+                        <p>I'm currently used C# .NET MVC, VB.NET, .NetCore, Nuxt.js, Flutter, Python, PHP</p>
+						<p>Now interesting in Solidity, Blockchain</p>
                     </div>
+					<a href="#portfolio" class="btn btn-circle page-scroll">
+						<i class="fa fa-angle-double-down animated"></i>
+					</a>
                 </div>
             </div>
         </div>
@@ -52,12 +70,19 @@
                 <?php
                     for ($i=0; $i<=24; $i++)
                     {
+                        //if ($i%3==0)
+                        //    echo '<div class="row">';
                         echo '<div class="col-lg-4 col-md-6 col-sm-6">
                                 <img class="img img-responsive" alt="my work" src="work/work',($i+1),'.png">
                             </div>';
+                        //if ($i%3==0)
+                        //    echo '</div>';
                     }
                 ?>
+				
             </div>
+			
+			
         </div>
     </section>
 
@@ -69,16 +94,25 @@
                 <p>You can contact me via the following channel</p>
                 <ul class="list-inline banner-social-buttons">
                     <li>
-                        <a target="_blank" href="https://www.linkedin.com/in/forgetz/" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                        <a target="_blank" href="https://linkedin.com/in/forgetz/" class="btn btn-default btn-lg btn-circle"><i class="fab fa-linkedin-in"></i><span class="network-name"></span></a>
                     </li>
                     <li>
-                        <a target="_blank" href="https://github.com/forgetz" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                        <a target="_blank" href="https://github.com/forgetz" class="btn btn-default btn-lg btn-circle"><i class="fab fa-github"></i><span class="network-name"></span></a>
                     </li>
                     <li>
-                        <a target="_blank" href="https://www.facebook.com/forgetz" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                        <a target="_blank" href="https://m.me/forgetz" class="btn btn-default btn-lg btn-circle"><i class="fab fa-facebook-messenger"></i><span class="network-name"></span></a>
                     </li>
+					<li>
+                        <a target="_blank" href="https://instagram.com/iamnnat" class="btn btn-default btn-lg btn-circle"><i class="fab fa-instagram"></i><span class="network-name"></span></a>
+                    </li>
+					
                 </ul>
             </div>
+			<div class="col-lg-8 col-lg-offset-2">
+				<a href="#intro" class="btn btn-circle page-scroll">
+					<i class="fa fa-angle-double-up animated"></i>
+				</a>
+			</div>
         </div>
     </section>
 
